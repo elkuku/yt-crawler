6a5102064178239ad12a5c6422609d783a0ba627
Nikolai Plath <der.el.kuku@gmail.com>
2020-08-31 09:56:38 -0500

    Add base assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import $ from 'jquery'
 
-// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap'
+import 'bootswatch/dist/sketchy/bootstrap.min.css';
+
+require('open-iconic/font/css/open-iconic-bootstrap.css')
+
 import '../css/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
