cc9e573bc2ee2d3517c62f62013d83446ffe51b1
Nikolai Plath <der.el.kuku@gmail.com>
2021-03-05 10:21:14 -0500

    Update tests

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -17,15 +17,11 @@ class UserControllerTest extends WebTestCase
 
         $userRepository = static::$container->get(UserRepository::class);
 
-        // retrieve the test user
         $testUser = $userRepository->findOneByEmail('user@example.com');
-
-        // simulate $testUser being logged in
         $client->loginUser($testUser);
 
-        // test e.g. the profile page
         $client->request('GET', '/');
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Hello user@example.com');
+        $this->assertSelectorTextContains('h4', 'Hello user@example.com');
     }
 }
