e5bb831456ffdf0656d828826a254ed0421d70ca
Nikolai Plath <der.el.kuku@gmail.com>
2021-05-10 08:42:57 -0500

    Add display Symfony version

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -16,6 +17,7 @@ class DefaultController extends AbstractController
             [
                 'controller_name' => 'DefaultController',
                 'php_version'     => PHP_VERSION,
+                'symfony_version' => Kernel::VERSION,
             ]
         );
     }
